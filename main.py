20ca8eb2505864c25ffa60b3389b5b09f79bfd97
kebatdungthu710 <63869828+kebatdungthu710@users.noreply.github.com>
2020-05-04 02:05:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ flags.DEFINE_string("OPER_NAME", "Experiment_6_21_6", "name of the experiment")
 flags.DEFINE_string("path", '?', "path of training data")
 flags.DEFINE_integer("batch_size", 4, "size of single batch")
 flags.DEFINE_integer("max_iters", 100000, "number of total iterations for G")
-flags.DEFINE_integer("learn_rate", 0.0001, "learning rate for g and d")
+flags.DEFINE_float("learn_rate", 0.0001, "learning rate for g and d")
 flags.DEFINE_integer("test_step", 34000, "loading setp model for testing")
 flags.DEFINE_boolean("is_load", False, "whether loading the pretraining model for training")
 flags.DEFINE_boolean("use_sp", True, "whether using spectral normalization")
 flags.DEFINE_integer("lam_recon", 1, "weight for recon loss")
 flags.DEFINE_integer("lam_gp", 10, "weight for gradient penalty")
-flags.DEFINE_integer("beta1", 0.5, "beta1 of Adam optimizer")
-flags.DEFINE_integer("beta2", 0.999, "beta2 of Adam optimizer")
+flags.DEFINE_float("beta1", 0.5, "beta1 of Adam optimizer")
+flags.DEFINE_float("beta2", 0.999, "beta2 of Adam optimizer")
 flags.DEFINE_integer("n_critic", 1, "iters of g for every d")
 
 FLAGS = flags.FLAGS
@@ -30,8 +30,8 @@ if __name__ == "__main__":
     print FLAGS.OPER_FLAG
 
     root_log_dir = "./outpout/log/logs{}".format(FLAGS.OPER_FLAG)
-    checkpoint_dir = "./outpout/model_gan{}/".format(FLAGS.OPER_NAME)
-    sample_path = "./outpout/sample{}/sample_{}".format(FLAGS.OPER_FLAG, FLAGS.OPER_NAME)
+    checkpoint_dir = "/content/drive/My Drive/GoogleColab/Checkpoint/model_gan{}/".format(FLAGS.OPER_NAME)
+    sample_path = "/content/drive/My Drive/GoogleColab/Sample/sample{}/sample_{}".format(FLAGS.OPER_FLAG, FLAGS.OPER_NAME)
 
     mkdir_p(root_log_dir)
     mkdir_p(checkpoint_dir)
@@ -51,5 +51,3 @@ if __name__ == "__main__":
     if FLAGS.OPER_FLAG == 1:
         eGan.build_test_model_GAN()
         eGan.test(test_step=FLAGS.test_step)
-
-
